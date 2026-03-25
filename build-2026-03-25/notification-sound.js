827d899ec6608547e43951d2eda24c0589ed589f
eugene <eugene.eighty20virtual@gmail.com>
2026-03-25 23:26:36 +0800

    Eugene > Frontend Build > build-2026-03-25

---
+++ build-2026-03-25/notification-sound.js
@@ -0,0 +1,22 @@
+// Simple notification sound generator
+window.playNotificationSound = () => {
+  try {
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const oscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+    
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+    
+    oscillator.frequency.value = 800; // Hz
+    oscillator.type = 'sine';
+    
+    gainNode.gain.setValueAtTime(0.3, audioContext.currentTime);
+    gainNode.gain.exponentialRampToValueAtTime(0.01, audioContext.currentTime + 0.3);
+    
+    oscillator.start(audioContext.currentTime);
+    oscillator.stop(audioContext.currentTime + 0.3);
+  } catch (error) {
+    console.log('Audio not supported');
+  }
+};
